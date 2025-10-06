de4c05c42d8166f78c6fbade903deba6f7a70521
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-10-06 09:36:09 -0300

    feat: add main module logic

---
+++ src/__main__.py
@@ -0,0 +1,13 @@
+#!/bin/env python
+''' Project to automate SharePoint form fill '''
+from helpers.bannershow import print_header_presentation
+from helpers.runchecker import instance_checker
+
+if __name__ == '__main__':
+    print_header_presentation()
+    instance_checker()
+    # TODO - Check update
+    # TODO - Apply update
+    # TODO - Check fillorders
+    # TODO - Startup webhandler
+    # TODO - Fill workorders
