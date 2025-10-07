3349e99b8ec0a44c5ee6e175d267ed59d4180669
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-10-07 15:04:43 -0300

    refactor: change import configurations to environment to a dictionary

--- src/helpers/constants.py
+++ src/helpers/constants.py
@@ -1,7 +1,7 @@
 ''' Module to hold constants and configurations values '''
 import os
 import sys
-from dotenv import load_dotenv
+from dotenv import dotenv_values
 
 # Set what environment it is
 DEV_ENV = not getattr(sys, 'frozen', False)
@@ -22,4 +22,8 @@ BASE_FOLDER = (
 
 # Load configuration file
 configuration_filepath = os.path.join(BASE_FOLDER, APPNAME + '.conf')
-load_dotenv(configuration_filepath)
+CONFIGS = dotenv_values(configuration_filepath)
+
+# Load webelements path file
+webelements_filepath = os.path.join(BASE_FOLDER, APPNAME + '.path')
+WAYPATH = dotenv_values(webelements_filepath)
