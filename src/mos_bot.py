dc519b83bdb89b1ade20ddd528b2012eafb171e3
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-10-07 15:10:22 -0300

    feat: add instruction to update chrome driver version

--- src/mos_bot.py
+++ src/mos_bot.py
@@ -2,12 +2,12 @@
 ''' Project to automate SharePoint form fill '''
 from helpers.bannershow import print_header_presentation
 from helpers.runchecker import instance_checker
+from helpers.updater import update_chromedriver
 
 if __name__ == '__main__':
     print_header_presentation()
     instance_checker()
-    # TODO - Check update
-    # TODO - Apply update
+    update_chromedriver()
     # TODO - Check fillorders
     # TODO - Startup webhandler
     # TODO - Fill workorders
