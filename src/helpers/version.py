15e69d6806984e99238fd40771c8f1e862a3e892
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-10-07 15:08:11 -0300

    feat: add property `extra` to handle four numbers of versions

--- src/helpers/version.py
+++ src/helpers/version.py
@@ -4,25 +4,26 @@ class Version:
     major: int
     minor: int
     patch: int
+    extra: int
     def __init__(self, *args: str | int) -> None:
-        # Accepts either (self, major, minor, patch) or (self, 'major.minor.patch')
-        if len(args) == 4:
+        # Accepts either (self, major, minor, patch, extra) or (self, 'major.minor.patch.extra')
+        if len(args) > 1 and all(isinstance(x, int) for x in args):
             # args[1], args[2], args[3] should be int or str convertible to int
             try:
-                self.major = int(args[1])
-                self.minor = int(args[2])
-                self.patch = int(args[3])
+                self.major = int(args[0])
+                self.minor = int(args[1])
+                self.patch = int(args[2]) if len(args) > 2 else 0
+                self.extra = int(args[3]) if len(args) > 3 else 0
             except (ValueError, TypeError) as e:
                 raise ValueError('Version arguments must be integers') from e
-        elif len(args) == 2 and isinstance(args[1], str):
-            # Accepts string like 'major.minor.patch'
-            argv = args[1].split('.', maxsplit=3)
-            if len(argv) < 3:
-                raise ValueError('Version string must be in format major.minor.patch')
+        elif len(args) == 1 and isinstance(args[0], str):
+            # Accepts string like 'major.minor.patch.extra'
+            argv = args[0].split('.')
             try:
                 self.major = int(argv[0])
                 self.minor = int(argv[1])
-                self.patch = int(argv[2])
+                self.patch = int(argv[2]) if len(argv) > 2 else 0
+                self.extra = int(argv[3]) if len(argv) > 3 else 0
             except (ValueError, TypeError) as e:
                 raise ValueError('Version string must contain only integers') from e
         else:
@@ -33,7 +34,8 @@ class Version:
         return (
             self.major == value.major and
             self.minor == value.minor and
-            self.patch == value.patch
+            self.patch == value.patch and
+            self.extra == value.extra
         )
     def __gt__(self, other: object) -> bool:
         if not isinstance(other, Version):
@@ -42,6 +44,8 @@ class Version:
             return self.major > other.major
         if self.minor != other.minor:
             return self.minor > other.minor
-        return self.patch > other.patch
+        if self.patch != other.patch:
+            return self.patch > other.patch
+        return self.extra > other.extra
     def __str__(self) -> str:
-        return str(self.major) + '.' + str(self.minor) + '.' + str(self.patch)
+        return str(self.major) + '.' + str(self.minor) + '.' + str(self.patch) + '.' + str(self.extra)
