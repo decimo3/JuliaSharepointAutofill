b00943d2f919fca458076f4ba9cacf47a1a7e40a
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-10-09 09:36:59 -0300

    feat: add module to setup, startup and get elements by webdriver

---
+++ src/webhandler/webhandler.py
@@ -0,0 +1,81 @@
+''' Module to setup and startup webdriver '''
+import os
+import datetime
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from helpers.constants import CONFIGS, WAYPATH, WAITSEC, BASE_FOLDER
+from helpers.dialogator import show_popup_error
+
+class ElementNotFoundException(Exception):
+    ''' Custon exception to indicate when element is not found '''
+
+BY = {
+    '#': By.ID,
+    '/': By.XPATH,
+    '.': By.CLASS_NAME
+}
+
+class WebHandler:
+    ''' Class to Wrap webdriver '''
+    def __init__(self) -> None:
+        siteurl = CONFIGS.get('WEBSITE', '')
+        chromepath = CONFIGS.get('GCHROME', '')
+        temppath = CONFIGS.get('TMPPATH', os.path.join(BASE_FOLDER, 'tmp'))
+        if not siteurl or not chromepath:
+            error_message = 'A configuração "GCHROME" ou "WEBSITE" não está acessível!'
+            show_popup_error(error_message)
+            raise ValueError(error_message)
+        driverpath = os.path.join(BASE_FOLDER, 'chromedriver-win64', 'chromedriver.exe')
+        if not os.path.exists(driverpath):
+            error_message = f'O programa "{driverpath}" não está acessível!'
+            show_popup_error(error_message)
+            raise FileNotFoundError(error_message)
+        service = Service(executable_path=driverpath)
+        options = webdriver.ChromeOptions()
+        options.binary_location = chromepath
+        options.add_argument(f'--app={siteurl}')
+        options.add_argument(f'--user-data-dir={temppath}')
+        options.add_argument(f'--unsafely-treat-insecure-origin-as-secure={siteurl}')
+        self.driver = webdriver.Chrome(service=service, options=options)
+        self.driver.maximize_window()
+    def get_elements(self, pathname: str, timeout: str) -> list[WebElement] | None:
+        ''' Function to get a list of WebElements '''
+        # Example 1: /html/body/main/form
+        # Will be By.XPATH and '/html/body/main/form'
+        # Example 2: #form-item
+        # Will be By.CLASS_NAME and 'form-item'
+        # Example 3: .form-id
+        # Will be By.ID and 'form-id'
+        pathvalue = WAYPATH.get(pathname, '')
+        if not pathvalue:
+            error_message = f'A caminho {pathname} não foi encontrado na configuração!'
+            show_popup_error(error_message)
+            raise ValueError(error_message)
+        bytype = BY.get(pathvalue[:1], '')
+        if not bytype:
+            error_message = f'O tipo do caminho {pathname} não pode ser definido!'
+            show_popup_error(error_message)
+            raise ValueError(error_message)
+        byvalue = pathvalue[1:] if bytype != By.XPATH else pathvalue
+        seconds = WAITSEC.get(timeout, 0)
+        expiration_time = datetime.datetime.now() + datetime.timedelta(seconds=seconds)
+        while True:
+            #try:
+            elements = self.driver.find_elements(bytype, byvalue)
+            if elements:
+                return elements
+            #except:
+            #    pass
+            if datetime.datetime.now() > expiration_time:
+                return None
+    def get_element(self, pathname: str, timeout: str) -> WebElement:
+        ''' Function to get a single WebElement '''
+        elements = self.get_elements(pathname, timeout)
+        if not elements:
+            error_message = f'O elemento {pathname} não foi encontrado!'
+            show_popup_error(error_message)
+            raise ElementNotFoundException(error_message)
+        return elements[0]
