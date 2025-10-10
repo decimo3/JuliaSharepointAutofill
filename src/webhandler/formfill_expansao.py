2892a9968b215bf576537b77d55afe87403325f9
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-10-10 10:27:22 -0300

    feat: add module to handle with fill `expansao` Sharepoint with an empty function

---
+++ src/webhandler/formfill_expansao.py
@@ -0,0 +1,6 @@
+''' Module to handle with fill `expansao` Sharepoint '''
+import pandas
+from .webhandler import WebHandler
+
+def formfill_expansao(infolist: pandas.DataFrame, filelist: list[str]) -> None:
+    ''' Function to handle with fill `expansao` Sharepoint '''
