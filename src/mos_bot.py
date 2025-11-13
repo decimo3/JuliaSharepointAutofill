8b3ef6a3181780b4c316a166a2c1a5bb376aa1d5
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-11-13 17:14:04 -0300

    feat: implement scrapper to main function

--- src/mos_bot.py
+++ src/mos_bot.py
@@ -3,12 +3,14 @@
 from helpers.bannershow import print_header_presentation
 from helpers.runchecker import instance_checker
 from helpers.updater import update_chromedriver
+from webhandler.scrapper import sharepoint_scrapper
 from webhandler.formfill import sharepoint_fillform
 
 if __name__ == '__main__':
     print_header_presentation()
     instance_checker()
     update_chromedriver()
+    sharepoint_scrapper()
     sharepoint_fillform()
     # TODO - Check fillorders
     # TODO - Startup webhandler
