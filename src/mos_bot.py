23c2a881a2bd4f74cc4cf9bc8a08e0ce15bd70b9
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-11-14 09:39:55 -0300

    feat: add configuration to change operation mode of program

--- src/mos_bot.py
+++ src/mos_bot.py
@@ -3,6 +3,8 @@
 from helpers.bannershow import print_header_presentation
 from helpers.runchecker import instance_checker
 from helpers.updater import update_chromedriver
+from helpers.constants import CONFIGS
+from helpers.dialogator import show_popup_error
 from webhandler.scrapper import sharepoint_scrapper
 from webhandler.formfill import SharePointFormFill
 
@@ -10,6 +12,11 @@ if __name__ == '__main__':
     print_header_presentation()
     instance_checker()
     update_chromedriver()
-    sharepoint_scrapper()
-    filler = SharePointFormFill()
-    filler.sharepoint_fillform()
+    mode = str(CONFIGS.get('OPERACAO', ''))
+    if mode == 'EXTRAIR':
+        sharepoint_scrapper()
+    elif mode == 'INSERIR':
+        filler = SharePointFormFill()
+        filler.sharepoint_fillform()
+    else:
+        show_popup_error('O modo de operação é inválido!')
