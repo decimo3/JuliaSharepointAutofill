0d0b51ea30741ba123a581e3c8a94141e929d7fc
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-11-14 09:32:37 -0300

    refactor: add timestamp to sharepoint report filename

--- src/webhandler/scrapper.py
+++ src/webhandler/scrapper.py
@@ -1,5 +1,6 @@
 ''' Module to scrapper Sharepoint forms '''
 from io import StringIO
+from datetime import datetime
 from pandas import DataFrame, read_html
 from webhandler.webhandler import WebHandler, ElementNotFoundException
 
@@ -18,8 +19,10 @@ def scrapper_manutencao(handler: WebHandler) -> DataFrame:
 def sharepoint_scrapper() -> None:
     ''' Main function to handle sharepoints scrapper '''
     with WebHandler('EXPANSAO_SHAREPOINT') as handler:
+        timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
         df = scrapper_expansao(handler)
-        df.to_csv('expansao.csv', index=False)
+        df.to_csv(f'expansao_{timestamp}.csv', index=False)
     with WebHandler('MANUTENCAO_SHAREPOINT') as handler:
+        timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
         df = scrapper_manutencao(handler)
-        df.to_csv('manutencao.csv', index=False)
+        df.to_csv(f'manutencao_{timestamp}.csv', index=False)
