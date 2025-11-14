e884a1407337b1a45ab5cb65618911233d09dcbc
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-11-14 09:31:38 -0300

    refactor: change direct access to elements to parse HTML

--- src/webhandler/scrapper.py
+++ src/webhandler/scrapper.py
@@ -1,28 +1,15 @@
 ''' Module to scrapper Sharepoint forms '''
-from time import sleep
-from pandas import DataFrame
-from selenium.webdriver.common.by import By
+from io import StringIO
+from pandas import DataFrame, read_html
 from webhandler.webhandler import WebHandler, ElementNotFoundException
 
 def scrapper_expansao(handler: WebHandler) -> DataFrame:
     ''' Function to scrapper `expansao` Sharepoint '''
-    handler.get_element('EXPANSAO_RELATORIO', 'TOTAL')
-    sleep(5)
-    # Collect header
-    head: list[str] = []
-    headers = handler.get_elements('EXPANSAO_CABECALHOS', 'AGORA')
-    if headers is None:
+    table = handler.get_element('EXPANSAO_RELATORIO', 'TOTAL')
+    html = table.get_attribute('innerHTML')
+    if html is None:
         raise ElementNotFoundException()
-    head = [col.text.strip() if col.text.strip() else f'Coluna{i}' for i, col in enumerate(headers)]
-    # Collect bodies
-    body: list[list[str]] = []
-    line_number = len(handler.get_elements('EXPANSAO_VALORACOES_LN', 'AGORA') or [])
-    for i in range(line_number):
-        cols: list[str] = []
-        for j in range(len(head)):
-            cols.append(handler.get_element('EXPANSAO_VALORACOES_XY', 'AGORA', i + 1, j + 1).text.strip())
-        body.append(cols)
-    return DataFrame(data=body, columns=head)
+    return read_html(StringIO(html))[1]
 
 def scrapper_manutencao(handler: WebHandler) -> DataFrame:
     ''' Function to scrapper `manutencao` Sharepoint '''
