298996402622154ed0d47d063850b4dbd055ff19
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-11-14 09:37:34 -0300

    fix: change direct method call to instantiate class first

--- src/mos_bot.py
+++ src/mos_bot.py
@@ -4,14 +4,12 @@ from helpers.bannershow import print_header_presentation
 from helpers.runchecker import instance_checker
 from helpers.updater import update_chromedriver
 from webhandler.scrapper import sharepoint_scrapper
-from webhandler.formfill import sharepoint_fillform
+from webhandler.formfill import SharePointFormFill
 
 if __name__ == '__main__':
     print_header_presentation()
     instance_checker()
     update_chromedriver()
     sharepoint_scrapper()
-    sharepoint_fillform()
-    # TODO - Check fillorders
-    # TODO - Startup webhandler
-    # TODO - Fill workorders
+    filler = SharePointFormFill()
+    filler.sharepoint_fillform()
