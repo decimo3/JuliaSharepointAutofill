7da224e4a85f7a2db2764161370c8a1ae9b647e6
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-11-17 14:32:21 -0300

    feat: add instruction to use custom folder or `odl` by default

--- src/webhandler/scrapper.py
+++ src/webhandler/scrapper.py
@@ -1,8 +1,10 @@
 ''' Module to scrapper Sharepoint forms '''
+import os
 from io import StringIO
 from datetime import datetime
 from pandas import DataFrame, read_html
 from webhandler.webhandler import WebHandler, ElementNotFoundException
+from helpers.constants import BASE_FOLDER, CONFIGS
 
 def scrapper_expansao(handler: WebHandler) -> DataFrame:
     ''' Function to scrapper `expansao` Sharepoint '''
@@ -18,11 +20,21 @@ def scrapper_manutencao(handler: WebHandler) -> DataFrame:
 
 def sharepoint_scrapper() -> None:
     ''' Main function to handle sharepoints scrapper '''
+    custom_path = str(CONFIGS.get('ODLPATH', ''))
+    report_path = (
+        custom_path
+        if custom_path else
+        os.path.join(BASE_FOLDER, 'odl')
+    )
+    if not os.path.exists(report_path):
+        os.mkdir(report_path)
     with WebHandler('EXPANSAO_SHAREPOINT') as handler:
         timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+        filepath = os.path.join(report_path, 'expansao_' + timestamp + '.csv')
         df = scrapper_expansao(handler)
-        df.to_csv(f'expansao_{timestamp}.csv', index=False)
+        df.to_csv(filepath, index=False)
     with WebHandler('MANUTENCAO_SHAREPOINT') as handler:
         timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+        filepath = os.path.join(report_path, 'manutencao_' + timestamp + '.csv')
         df = scrapper_manutencao(handler)
-        df.to_csv(f'manutencao_{timestamp}.csv', index=False)
+        df.to_csv(filepath, index=False)
